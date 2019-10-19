de00724153544043164c61e5ad366d6fc93e8681
Usamath PC <usamathpallikkachalil@gmail.com>
2019-10-19 16:11:41 +0530

    Updates to App
    1. Added update method to Enemy prototype.
    
    2. Defined Player constructor and defined properties.

--- js/app.js
+++ js/app.js
@@ -1,25 +1,17 @@
 // Enemies our player must avoid
 var Enemy = function(x, y, speed) {
-  // Variables applied to each of our instances go here,
-  // we've provided one for you to get started
-
-  // The image/sprite for our enemies, this uses
-  // a helper we've provided to easily load images
   this.x = x;
   this.y = y;
   this.speed = speed;
   this.sprite = "images/enemy-bug.png";
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
+//Update position x
 Enemy.prototype.update = function(dt) {
-  // You should multiply any movement by the dt parameter
-  // which will ensure the game runs at the same speed for
-  // all computers.
+  this.x = this.speed * dt;
 };
 
-// Draw the enemy on the screen, required method for game
+// Draw the enemy on the screen
 Enemy.prototype.render = function() {
   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
@@ -27,6 +19,11 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+
+let Player = function(x, y) {
+  Enemy.call(this, x, y);
+  this.sprite = "images/char-boy.png";
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
