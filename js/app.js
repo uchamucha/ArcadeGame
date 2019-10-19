72b9301c20884f8b7a6549cd466cb342e7c2d874
Usamath PC <usamathpallikkachalil@gmail.com>
2019-10-19 23:35:44 +0530

    Added comments. FIN!

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-// Enemies our player must avoid
+// Bugs constructor
 var Enemy = function(x, y) {
   this.x = x;
   this.y = y;
@@ -9,34 +9,41 @@ var Enemy = function(x, y) {
   this.sprite = "images/enemy-bug.png";
 };
 
-//Update position x
+//Update bug position along x-axis
 Enemy.prototype.update = function(dt) {
+  //make bugs move
   this.x += this.speed * dt;
 
+  //make bugs reappear head first
   if (this.x > 500) {
     this.x = -100;
     this.speed = this.randomizer();
   }
 };
 
-// Draw the enemy on the screen
+// Render bugs on the screen
 Enemy.prototype.render = function() {
   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+// Create array to hold bug instances
 let allEnemies = new Array();
 
-//observed 83 to be optimal Y axis separation by trial and error
+// Instantiate three bugs and store them in the array
 allEnemies[0] = new Enemy(0, 62);
 allEnemies[1] = new Enemy(0, 145);
 allEnemies[2] = new Enemy(0, 228);
 
+// Player constructor
 let Player = function(x, y) {
+  // Using call on Enemy constructor to keep things DRY
   Enemy.call(this, x, y);
   this.sprite = "images/char-boy.png";
 };
 
+// Update Player position on the canvas
 Player.prototype.update = function() {
+  // Collision detection!
   allEnemies.forEach(function(bug) {
     if (
       player.x - bug.x < 80 &&
@@ -48,6 +55,7 @@ Player.prototype.update = function() {
     }
   });
 
+  // Reset player after reaching river
   if (player.y < 0) {
     setTimeout(() => {
       player.x = 200;
@@ -55,9 +63,13 @@ Player.prototype.update = function() {
     }, 250);
   }
 };
+
+// Render player on the screen
 Player.prototype.render = function() {
   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
+
+// Add method to handle input to change player
 Player.prototype.handleInput = function(key) {
   if (key == "up" && this.y > 0) {
     this.y -= 83;
@@ -73,8 +85,10 @@ Player.prototype.handleInput = function(key) {
   }
 };
 
+// Instantiate player
 let player = new Player(200, 405);
 
+// Add eventlisteners for keys
 document.addEventListener("keyup", function(e) {
   var allowedKeys = {
     37: "left",
