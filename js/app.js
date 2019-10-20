f5d0862c9ea13d986e1c1d6f007f3913b086fdb9
Usamath PC <usamathpallikkachalil@gmail.com>
2019-10-20 10:46:34 +0530

    Review: Removed redundancies in winning logic.
    Moved it to update() in player proto

--- js/app.js
+++ js/app.js
@@ -54,6 +54,14 @@ Player.prototype.update = function() {
       player.y = 405;
     }
   });
+
+  //Reset player after reaching river
+  if (this.y < 0) {
+    setTimeout(() => {
+      this.x = 200;
+      this.y = 405;
+    }, 250);
+  }
 };
 
 // Render player on the screen
@@ -71,14 +79,6 @@ Player.prototype.handleInput = function(key) {
     this.x -= 100;
   } else if (key == "right" && this.x < 400) {
     this.x += 100;
-  }
-
-  //Reset player after reaching river
-  else if (this.y < 0) {
-    setTimeout(() => {
-      player.x = 200;
-      player.y = 405;
-    }, 250);
   }
 };
 
